3d75823b9cb40ae0c1a1a4331b877342f32ae224
David McCormack <David.mccormack07@gmail.com>
2023-04-26 16:27:20 +0100

    Making Word File

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -1,24 +1,55 @@
 package ie.tudublin;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Word {
     
+    private String word;
+
+    ArrayList<Follow>follow = new ArrayList<Follow>();
 
 
 
+    //getters and setters for the array list
+    public ArrayList<Follow> getFollow() {
+        return follow;
+    }
 
+    public void setFollow(ArrayList<Follow> follow) {
+        this.follow = follow;
+    }
 
+    //Constructors for the array list
+    public Word(ArrayList<Follow> follow) {
+        this.follow = follow;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    //to string for the array list 
+    @Override
+    public String toString() {
+        return "Word [follow=" + follow + "]";
+    }
 
     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
